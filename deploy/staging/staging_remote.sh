33daf283202096b9532c3eb4ee3bba7e836882fd
James Smithies <jsmithies@gmail.com>
2025-05-29 20:47:01 +1000

    [WIP] Staging, no telemetry

--- deploy/staging/staging_remote.sh
+++ deploy/staging/staging_remote.sh
@@ -350,7 +350,22 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
+    # WebSocket proxy configuration
     location /ws {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 300s;
+        proxy_send_timeout 300s;
+    }
+
+    # Original WebSocket location (keeping for compatibility)
+    location /ws/ {
         proxy_pass http://127.0.0.1:8000/ws;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
