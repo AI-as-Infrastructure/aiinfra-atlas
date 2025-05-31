68fdb28d5da6ba04ea28829f5c180fbf57c9b949
James Smithies <jsmithies@gmail.com>
2025-06-01 02:24:34 +1000

    Remove deprecated authService.js

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -8,7 +8,6 @@ import {
   login as cognitoLogin,
   logout as cognitoLogout
 } from '../auth/amplify-auth';
-import { logout as authLogout } from '../auth/authService';
 
 export const useAuthStore = defineStore('auth', () => {
   // State
@@ -117,7 +116,7 @@ export const useAuthStore = defineStore('auth', () => {
   }
 
   async function logout() {
-    authLogout();
+    await cognitoLogout();
   }
 
   // Initialize auth state when store is first created
